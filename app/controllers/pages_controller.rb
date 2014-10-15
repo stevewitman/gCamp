2ca7e5263e4f3b1310447df760d2292b6052a981
Steve Witman <stevewitman@gmail.com>
2014-10-15 15:04:16 -0600

    added FAQ model and FAQ to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,4 +18,22 @@ class PagesController < ApplicationController
 
   end
 
+  def faq
+
+    faq1 = Faq.new
+    faq1.question = "What is gCamp?"
+    faq1.answer = "gCamp is an qwesome tool that is going to change your life.  gCamp os your one stop shop to organize all your tasks and documents.  You'll also be able to track comments that you and others make.  gCamp may eventually replace all need for paper and pens in the entire world.  Well, maybe not, but it is going to be pretty cool."
+
+    faq2 = Faq.new
+    faq2.question = "How do I join gCamp?"
+    faq2.answer = "Begin your application <here (make this a link)>"
+
+    faq3 = Faq.new
+    faq3.question = "When will gCamp be finished?"
+    faq3.answer = "Answer here ..."
+
+    @faq = [faq1, faq2, faq3]
+
+  end
+
 end
