c7cd8dc3b15386d96411c16e40ae2eba3df85afd
Steve Witman <stevewitman@gmail.com>
2014-10-15 13:40:24 -0600

    refactored quotes into the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController < ApplicationController
 
+  def index
+    @quotes = [
+                ["\"Failure is not an option, Everyone has to Succeed\"", "Arnold Scwarzenegger"],
+                ["\"Your time is limited, so don\'t waste it living someone else's life.\"", "Steve Jobs"],
+                ["\"Better Ingredients, Better Pizza\"", "Papa John"]
+              ]
+  end
+
 end
