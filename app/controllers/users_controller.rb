fe33dd20523917a28d0a6ac2fe290b10e83fc887
Steve Witman <stevewitman@gmail.com>
2014-10-28 10:30:27 -0600

    added basic edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -12,7 +13,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.save
     redirect_to users_path, notice: 'User was sucessfully created'
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
