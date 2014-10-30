cf3af1268773299657870d3d2878545142ecd3a7
Steve Witman <stevewitman@gmail.com>
2014-10-30 11:51:24 -0600

    finished destroy user from edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
   private
     def set_user
       @user = User.find(params[:id])
