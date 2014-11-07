94902753ab33de38bd169d4e59a00e58e5c7814e
Steve Witman <stevewitman@gmail.com>
2014-11-07 15:48:15 -0700

    added specs for projects

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature "projects" do
+
+  scenario "users can make new project" do
+    visit '/'
+    click_on "Projects"
+    click_on "Create Project"
+    expect(page).to have_content("Create project")
+    fill_in "Name", with: "TestProject"
+    click_on "Create Project"
+    # user can view show page
+    expect(page).to have_content("TestProject")
+    click_on "Edit"
+    fill_in "Name", with: "TestProject2"
+    click_on "Update Project"
+    expect(page).to have_content("TestProject2")
+    click_on "Destroy"
+    expect(page).to have_content("Project was sucessfully deleted")
+  end
+
+
+
+
+
+end
