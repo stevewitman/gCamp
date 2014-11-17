5993301f51fda0a3a37aae3a3284c253cd9d38b6
Steve Witman <stevewitman@gmail.com>
2014-11-17 14:53:29 -0700

    removed db folder from spec/models

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,8 +6,19 @@ describe Task do
                     due_date: Date.today - 2.days)
     task.valid?
     expect(task.errors[:due_date].present?).to eq(true)
+  end
 
-# need to test that task can be edited with due_date in the past
+  it 'validate a task cannot be created with a date in the past' do
+    travel_to 1.day.ago do
+      task = Task.new(description: "test description",
+                      due_date: Date.today)
+      expect(task.valid?).to be(true)
+      task.save
+      expect(task.valid?).to be(true)
+
+    end
+# return to the present
+
 
   end
 end
