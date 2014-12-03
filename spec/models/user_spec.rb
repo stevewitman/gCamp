e956fdf04a7b0e6f88120b54b28249d1c20428c7
Steve Witman <stevewitman@gmail.com>
2014-12-03 15:45:37 -0700

    made task and user model specs more specific

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,16 +8,17 @@ describe User do
                 email: "test@mail.com",
                 password: "test",
                 password_confirmation: "test")
+    # create another user with same email but w/some caps
     user2 = User.new(
                 first_name: "testfirst2",
                 last_name: "testlast2",
                 email: "TEST@mail.com",
                 password: "test",
                 password_confirmation: "test")
-    # expect(user2.valid?).to be(false)
     user2.valid?
-    expect(user2.errors[:email.present?).to eq(true)
-
+    # this should give "has already been taken"
+    expect(user2.errors[:email]).to eq(["has already been taken"])
+    # show that we can create that same user with a unique email
     user2.email = "test2@mail.com"
     expect(user2.valid?).to be(true)
   end
