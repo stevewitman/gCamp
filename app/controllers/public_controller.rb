4ec9ededced6e9d80252b654349f0bc5b05efb79
Steve Witman <stevewitman@gmail.com>
2014-12-22 19:08:38 -0700

    moved reseed method to public controller

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,3 +1,8 @@
 class PublicController < ApplicationController
   skip_before_action :require_login
+
+  def seed_db
+    Rails.application.load_seed
+    redirect_to root_path, notice: 'Successfully re-seeded database'
+  end
 end
